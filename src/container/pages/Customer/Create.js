0f53fcb3e16af12ac516993a86469cd7039fa95b
ilyas <ilyas@mewahniagajaya.com>
2021-01-27 13:50:08 +0700

    add form create customers

--- src/container/pages/Customer/Create.js
+++ src/container/pages/Customer/Create.js
@@ -9,7 +9,172 @@ const CustomerCreate = () => {
             </div>
 
             <div className="row">
-                <h1>Form Create</h1>
+                <div className="col-xs-12 col-md-12 col-sm-12">
+                    <div className="panel panel-default">
+                        <div className="panel-body">
+                            <div className="alert alert-info text-center" role="alert">
+                                Perhatian!<br />
+                                Tambah Jobs Isikan Sesuai Dengan Customers
+              </div>
+
+                            {/* Content */}
+
+                            <div className="col-xs-12 col-md-6 col-sm-6">
+                                <div className="widget-box">
+                                    <div className="widget-body">
+                                        <div className="widget-main">
+                                            <div>
+                                                <label htmlFor="form-field-mask-1">
+                                                    Code
+                                                </label>
+                                                <div className="input-group">
+                                                    <input className="form-control input-mask-date" type="text" id="form-field-mask-1" />
+                                                    <span className="input-group-btn">
+                                                        <button className="btn btn-sm btn-default" type="button">
+                                                            <i className="ace-icon fa fa-calendar bigger-110" />
+                                                            Code Customer
+                                                        </button>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <hr />
+                                            <div>
+                                                <label htmlFor="form-field-mask-2">
+                                                    Customer Nama
+                                                </label>
+                                                <div className="input-group">
+                                                    <span className="input-group-addon">
+                                                        <i className="ace-icon fa fa-phone" />
+                                                    </span>
+                                                    <input className="form-control input-mask-phone" type="text" id="form-field-mask-2" />
+                                                </div>
+                                            </div>
+                                            <hr />
+                                            <div>
+                                                <label htmlFor="form-field-mask-3">
+                                                    Nomor Pelanggan
+                                                </label>
+                                                <div className="input-group">
+                                                    <input className="form-control input-mask-product" type="text" id="form-field-mask-3" />
+                                                    <span className="input-group-addon">
+                                                        <i className="ace-icon fa fa-key" />
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <hr />
+                                            <div>
+                                                <label htmlFor="form-field-mask-4">
+                                                    Phone
+            <small className="text-info">~9.99 ~9.99 999</small>
+                                                </label>
+                                                <div>
+                                                    <input className="input-medium input-mask-eyescript" type="text" id="form-field-mask-4" />
+                                                </div>
+                                            </div>
+                                            <hr />
+                                            <div>
+                                                <label htmlFor="form-field-mask-4">
+                                                    Fax
+            <small className="text-info">~9.99 ~9.99 999</small>
+                                                </label>
+                                                <div>
+                                                    <input className="input-medium input-mask-eyescript" type="text" id="form-field-mask-4" />
+                                                </div>
+                                            </div>
+                                            <hr />
+                                            <div>
+                                                <label htmlFor="form-field-mask-4">
+                                                    Mobile Phone
+            <small className="text-info">~9.99 ~9.99 999</small>
+                                                </label>
+                                                <div>
+                                                    <input className="input-medium input-mask-eyescript" type="text" id="form-field-mask-4" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-xs-12 col-md-6 col-sm-6">
+                                <div className="widget-box">
+                                    <div className="widget-body">
+                                        <div className="widget-main">
+                                            <div>
+                                                <label htmlFor="form-field-mask-1">
+                                                    Short Name
+                                                </label>
+                                                <div className="input-group">
+                                                    <input className="form-control input-mask-date" type="text" id="form-field-mask-1" />
+                                                    <span className="input-group-btn">
+                                                        <button className="btn btn-sm btn-default" type="button">
+                                                            <i className="ace-icon fa fa-calendar bigger-110" />
+                                                            address
+                                                        </button>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <hr />
+                                            <div>
+                                                <label htmlFor="form-field-mask-2">
+                                                address
+                                                </label>
+                                                <div className="input-group">
+                                                    <span className="input-group-addon">
+                                                        <i className="ace-icon fa fa-phone" />
+                                                    </span>
+                                                    <input className="form-control input-mask-phone" type="text" id="form-field-mask-2" />
+                                                </div>
+                                            </div>
+                                            <hr />
+                                            <div>
+                                                <label htmlFor="form-field-mask-3">
+                                                city
+                                                </label>
+                                                <div className="input-group">
+                                                    <input className="form-control input-mask-product" type="text" id="form-field-mask-3" />
+                                                    <span className="input-group-addon">
+                                                        <i className="ace-icon fa fa-key" />
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <hr />
+                                            <div>
+                                                <label htmlFor="form-field-mask-4">
+                                                post code
+            <small className="text-info">~9.99 ~9.99 999</small>
+                                                </label>
+                                                <div>
+                                                    <input className="input-medium input-mask-eyescript" type="text" id="form-field-mask-4" />
+                                                </div>
+                                            </div>
+                                            <hr />
+                                            <div>
+                                                <label htmlFor="form-field-mask-4">
+                                                director name
+                                                </label>
+                                                <div>
+                                                    <input className="input-medium input-mask-eyescript" type="text" id="form-field-mask-4" />
+                                                </div>
+                                            </div>
+                                            <hr />
+                                            <div>
+                                                <label htmlFor="form-field-mask-4">
+                                                Employee name
+                                                </label>
+                                                <div>
+                                                    <input className="input-medium input-mask-eyescript" type="text" id="form-field-mask-4" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
